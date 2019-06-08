b406851fb080998dd199d292506f75e608c5f60c
Geraldo Kandonga <10811063+geraldokandonga@users.noreply.github.com>
2019-06-08 22:27:38 +0200

    Completed large hero block intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
+
+
+gulp.task('watch', () => {
+
+  browserSync.init({
+  	server: {
+  		baseDir: "app"
+  	}
+  });
+
+  gulp.watch('./app/index.html', ()=>{
+  	 browserSync.reload();
+  });
+  
+  gulp.watch('./app/assets/styles/**/*.css')
+   .on('change', gulp.series('cssInject'));
+
+});
+
+gulp.task('cssInject', gulp.series('styles'), ()=> {
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});
